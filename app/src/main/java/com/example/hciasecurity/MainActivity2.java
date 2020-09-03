9fa9eddb64048b5fffa56a7f303142987001e714
Abdo <abdopetroleum@gmail.com>
2020-09-03 23:58:52 +0200

    my commit

--- app/src/main/java/com/example/hciasecurity/MainActivity2.java
+++ app/src/main/java/com/example/hciasecurity/MainActivity2.java
@@ -30,11 +30,7 @@ public class MainActivity2 extends AppCompatActivity {
         mainView=findViewById(R.id.my_layout);
         QuestionsFillers.initialize();
         completeExam=findViewById(R.id.center);
-        smallExam=findViewById(R.id.small_exam);
-        TrueOrFalseQiz=findViewById(R.id.t_fQuiz);
-        SCQuiz=findViewById(R.id.sc_quiz);
-        MCQuiz=findViewById(R.id.mc_quiz);
-        showAll=findViewById(R.id.all_questions);
+
         completeExam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,56 +42,6 @@ public class MainActivity2 extends AppCompatActivity {
                 startActivity(intent,ActivityOptions.makeSceneTransitionAnimation(MainActivity2.this).toBundle());
             }
         });
-        smallExam.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final Bundle bundle
-                        =new Bundle();
-                bundle.putInt("userOrder",2);
-                Intent intent=new Intent(MainActivity2.this,Exam.class);
-                intent.putExtras(bundle);
-                startActivity(intent,ActivityOptions.makeSceneTransitionAnimation(MainActivity2.this).toBundle());
-            }
-        });
-        TrueOrFalseQiz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final Bundle bundle
-                        =new Bundle();
-                bundle.putInt("userOrder",3);
-                Intent intent=new Intent(MainActivity2.this,Exam.class);
-                intent.putExtras(bundle);
-                startActivity(intent,ActivityOptions.makeSceneTransitionAnimation(MainActivity2.this).toBundle());
-            }
-        });
-        SCQuiz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final Bundle bundle
-                        =new Bundle();
-                bundle.putInt("userOrder",4);
-                Intent intent=new Intent(MainActivity2.this,Exam.class);
-                intent.putExtras(bundle);
-                startActivity(intent,ActivityOptions.makeSceneTransitionAnimation(MainActivity2.this).toBundle());
-            }
-        });
-        MCQuiz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final Bundle bundle
-                        =new Bundle();
-                bundle.putInt("userOrder",5);
-                Intent intent=new Intent(MainActivity2.this,Exam.class);
-                intent.putExtras(bundle);
-                startActivity(intent,ActivityOptions.makeSceneTransitionAnimation(MainActivity2.this).toBundle());
-            }
-        });
-        showAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity2.this,AllQuestgionsActivity.class);
-                startActivity(intent,ActivityOptions.makeSceneTransitionAnimation(MainActivity2.this).toBundle());
-            }
-        });
+       
     }
 }
